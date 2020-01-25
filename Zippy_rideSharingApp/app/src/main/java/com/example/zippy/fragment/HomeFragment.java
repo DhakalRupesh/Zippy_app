1d0f7280677b3fac64bfdb941f59857f3efb50b7
DHAKAL <aayushdhakal544@Gmail.com>
2020-01-25 12:18:17 +0545

    map implementation

--- Zippy_rideSharingApp/app/src/main/java/com/example/zippy/fragment/HomeFragment.java
+++ Zippy_rideSharingApp/app/src/main/java/com/example/zippy/fragment/HomeFragment.java
@@ -1,26 +1,91 @@
 package com.example.zippy.fragment;
 
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import com.example.zippy.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+     GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleMap gMap;
+    private GoogleApiClient googleApiClient;
+    private AutoCompleteTextView etSearchText;
+    private Button btnSearch;
+    private LocationRequest locationRequest;
+
+    private Marker currentUserLocationMarker;
+    private static final int Request_user_location_code = 99;
+    CameraUpdate center, zoom;
+    private Location previousLocation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        
         return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }

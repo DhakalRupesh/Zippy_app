0639de634be1c0df1ed07845682d251e3cfe3f56
DHAKAL <aayushdhakal544@Gmail.com>
2020-01-26 08:10:48 +0545

    locationaccessadded

--- Zippy_rideSharingApp/app/src/main/java/com/example/zippy/fragment/HomeFragment.java
+++ Zippy_rideSharingApp/app/src/main/java/com/example/zippy/fragment/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.zippy.fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Build;
@@ -8,6 +10,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -19,7 +23,9 @@ import android.widget.Button;
 import com.example.zippy.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,13 +33,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Marker;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
-     GoogleApiClient.OnConnectionFailedListener, LocationListener {
+     GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private GoogleMap gMap;
     private GoogleApiClient googleApiClient;
     private AutoCompleteTextView etSearchText;
     private Button btnSearch;
     private LocationRequest locationRequest;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     private Marker currentUserLocationMarker;
     private static final int Request_user_location_code = 99;
@@ -50,8 +57,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
-
-
+        mapFragment.getMapAsync(this);
 
         return view;
     }
@@ -68,7 +74,15 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this);
     }
 
     @Override
